5e64f90a224189167cb13d2d97f904e870de29da
Daan <daniel.jesus@stoom.com.br>
2020-06-29 03:20:27 -0300

    fixing db error

--- src/controllers/driverController.js
+++ src/controllers/driverController.js
@@ -3,62 +3,62 @@ var Driver = require('../models/Driver');
 const mongoose = require('mongoose');
 
 exports.get = async (req, res) => {
-    if (checkDbStatus){
-        await Driver.find(function (err,drivers) {
-            if (err) {
-                return res.status(500).send('Error: ',err);
-            }
-            //console.log(drivers);
-            return res.json(drivers);
-        });
-        
-       /* if (drivers.length > 0){
-            return res.status(404).send('No data found');
-        }*/
-        
-    }
-    return res.status(500).send('Database error');
+    await Driver.find(function (err,drivers) {
+        if (err) {
+            return res.status(500).send('Error: ',err);
+        }
+        //console.log(drivers);
+        return res.json(drivers);
+    });
+    
+    /* if (drivers.length > 0){
+        return res.status(404).send('No data found');
+    }*/
+     
 };
 
 exports.post = async (req, res) => {
-    if (checkDbStatus){
-        const { name, surname, cpf, date_of_birth } = req.body;
-        const newUuid = uuid();
-        const newDriver = await Driver.create({
-            newUuid,
-            name,
-            surname,
-            cpf,
-            date_of_birth,
-        });
+    const { name, surname, cpf, date_of_birth } = req.body;
+    const newUuid = uuid();
+    const newDriver = await Driver.create({
+        newUuid,
+        name,
+        surname,
+        cpf,
+        date_of_birth,
+    });
 
-        newDriver.save();
+    newDriver.save();
 
-        Driver.findById(newUuid, function (err) {
-            if (err) {
-                return res.status(500).send('Error: ',);
-            }
-        });
-        return res.status(201).send(`Driver created successfully!`);
-    }
-    return res.status(500).send('Database error');
+    Driver.findById(newUuid, function (err) {
+        if (err) {
+            return res.status(500).send('Error: ',);
+        }
+    });
+    return res.status(201).send(`Driver created successfully!`);
+};
+
+exports.put = async (req, res) => {
+    const { name, surname, cpf, date_of_birth } = req.body;
+    Driver.findById(req.params.id,function (err,driver) {
+        if (err) {
+            return res.status(500).send('Error: ',err);
+        }
+        driver.name = name;
+        driver.surname = surname;
+        driver.cpf = cpf;
+        driver.date_of_birth = date_of_birth;
+        driver.active = true;
+        driver.modification_date = new Date();
+        driver.save();
+        res.status(200).send('\nSuccessfully edited!\n');
+    });
 };
 
 exports.delete = async (req, res) => {
-    if(checkDbStatus){
-        const { id } = req.params.id;
-        const driver = await Driver.findById(id);
-        driver.active = false;
-        driver.save();
-        res.status(200).send('\n',driverName,' successfully deleted! ${id}\n');
-
-    }
-    return res.status(500).send('Database error');
+    const driver = await Driver.findById(req.params.id);
+    driver.active = false;
+    driver.modification_date = new Date();
+    await driver.save();
+    res.status(200).send('\nSuccessfully deleted!\n');
 };
-
-function checkDbStatus(){
-    if (mongoose.connection.readyState == 1){
-        return true;
-    }
-    return false;
-}
